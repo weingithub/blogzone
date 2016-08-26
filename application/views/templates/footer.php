33a2d1f2d368fd2ce1c4053c94c923d150dddf58
root <root@localhost.localdomain>
2016-08-26 02:50:32 -0700

    test

--- application/views/templates/footer.php
+++
@@ -1,9 +0,0 @@
-        <div id="footer">
-            <p>Copyright©2016 by coolcity</p>
-        </div>
-    </body>
-
-    <script src= "resource/jquery-3.1.0.js"></script>
-    <!-- <script src="resource/jquery-3.1.0.js"></script>  -->
-    <script src="resource/basic.js"></script>
-</html>
