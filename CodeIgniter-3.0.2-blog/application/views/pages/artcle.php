90875f9223592d4a800a8dbe52d84ca90b1f9928
weingithub <414267996@qq.com>
2017-01-23 18:56:51 +0800

    modify single article layout format

--- CodeIgniter-3.0.2-blog/application/views/pages/artcle.php
+++ CodeIgniter-3.0.2-blog/application/views/pages/artcle.php
@@ -2,23 +2,24 @@
 
             <div class="main-content">
                 <div class="article_page">
-                <h1><?php echo $title; ?></h1>
-                
-                  <div class="article_info">
-                    <span><?php echo $times; ?></span>
-                    <span>post by <?php echo $userid; ?></span>
-                <?php
-                    if (isset($_SESSION['username']) && $_SESSION['username'] == $userid)
-                    {
-                        echo '<a href="blog/write/'.$id.'">编辑</a>';
-                        echo '<a href="backend/delete/'.$id.'" style="margin-left:20px;">删除</a>';
-                    }
+                    <div class="article_page_title">
+                         <h1><?php echo $title; ?></h1>
+                    </div>
+                    <div class="article_info">
+                        <span><?php echo $times; ?></span>
+                        <span class="author">post by <?php echo $userid; ?></span>
+                    <?php
+                        if (isset($_SESSION['username']) && $_SESSION['username'] == $userid)
+                        {
+                            echo '<a href="blog/write/'.$id.'">编辑</a>';
+                            echo '<a href="backend/delete/'.$id.'" style="margin-left:20px;">删除</a>';
+                        }
                     
-                ?>
-                </div>                
+                    ?>
+                    </div>                
 
-                <HR width="100%" SIZE=1/>               
-                <?php echo $content; ?>   
+                    <HR width="100%" SIZE=1/>               
+                    <?php echo $content; ?>   
                 </div>
             </div>
 <br style="clear:both;" />  
