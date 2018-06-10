39f6e03432e7c1fa372e348e6b2d69518dec9134
codessheng <yr_c1990@163.com>
2018-06-10 22:20:33 +0800

    forget to submit changes about site search

--- CodeIgniter-3.0.2-blog/application/views/templates/left.php
+++ CodeIgniter-3.0.2-blog/application/views/templates/left.php
@@ -4,13 +4,18 @@
         <p>&nbsp LABEL</p>
         <ul>
          <?php foreach ($tags as $tag_item): ?>
-            <li> <a href="<?php echo site_url('blog/tag/'.$tag_item['id']); ?>"> <?php echo $tag_item["tagname"] ?></a></li> 
+                        <li> <a href="<?php echo site_url('blog/tag/'.$tag_item['id']); ?>"> <?php echo $tag_item["tagname"] ?></a> (<?php echo $tag_item['num'] ?>)</li> 
              <?php endforeach; ?>
         </ul>
         <br>
     </div>
-    <div class="popular">
-    
+    <div class="left-tag">
+        <p>Search</p>
+        <form method=post action="blog/searchinside">
+        <input id='search' size="15" <?php if ($keyword != "") echo "value=$keyword" ?> name="keyword">
+        <input type="submit" value="’æƒ⁄À—À˜">
+        <!-- <span class="search_button"> </span> <a href="tool/vocabulary">µ•¥ ≤È—Ø</a> -->
+        </form>
     </div>
 
 </div>
